7f5ff3d93f6813e2647af3a1a65378ad34735fe0
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-13 17:53:39 +0630

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,34 @@
 // Vid-5 : Multiple Components
 
+// const Home = () => {
+//     return ( 
+//         <div className="home">
+//             <h2>Homepage</h2>
+//         </div>
+//      );
+// }
+ 
+// export default Home;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//vid-7 - click events
+
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello '+ name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('mario',e)}>Click me again</button>
         </div>
      );
 }
