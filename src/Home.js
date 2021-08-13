60a10fa0c4635f784ac43d710645db198d89ae0a
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-13 19:04:44 +0630

    using state

--- src/Home.js
+++ src/Home.js
@@ -14,21 +14,48 @@
 
 //vid-7 - click events
 
+// const Home = () => {
+
+//     const handleClick = (e) => {
+//         console.log('hello, ninjas', e);
+//     }
+
+//     const handleClickAgain = (name, e) => {
+//         console.log('hello '+ name, e.target);
+//     }
+
+//     return ( 
+//         <div className="home">
+//             <h2>Homepage</h2>
+//             <button onClick={handleClick}>Click me</button>
+//             <button onClick={(e) => handleClickAgain('mario',e)}>Click me again</button>
+//         </div>
+//      );
+// }
+ 
+// export default Home;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//vid-8 - Using state(useState hook)
+
+import { useState } from 'react';
+
 const Home = () => {
-
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e);
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log('hello '+ name, e.target);
+    // let name = 'mario';
+    const [name, setName] = useState('mario'); //the first value is this initial value , can call what we want, sec value is a function which we can use to change that value
+    const [age, setAge] = useState(25);
+    const handleClick = () => {
+        setName('luigi');
+        setAge(30);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('mario',e)}>Click me again</button>
+            
         </div>
      );
 }
