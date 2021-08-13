05129ca6c4ad6cc1b523a352f9c384a252ad1b46
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-13 21:12:50 +0630

    Outputting Lists

--- src/Home.js
+++ src/Home.js
@@ -39,23 +39,50 @@
 
 //vid-8 - Using state(useState hook)
 
+// import { useState } from 'react';
+
+// const Home = () => {
+//     // let name = 'mario';
+//     const [name, setName] = useState('mario'); //the first value is this initial value , can call what we want, sec value is a function which we can use to change that value
+//     const [age, setAge] = useState(25);
+//     const handleClick = () => {
+//         setName('luigi');
+//         setAge(30);
+//     }
+
+//     return ( 
+//         <div className="home">
+//             <h2>Homepage</h2>
+//             <p>{ name } is { age } years old</p>
+//             <button onClick={handleClick}>Click me</button>
+            
+//         </div>
+//      );
+// }
+ 
+// export default Home;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//vid-10 - Outputting Lists
+
 import { useState } from 'react';
 
 const Home = () => {
-    // let name = 'mario';
-    const [name, setName] = useState('mario'); //the first value is this initial value , can call what we want, sec value is a function which we can use to change that value
-    const [age, setAge] = useState(25);
-    const handleClick = () => {
-        setName('luigi');
-        setAge(30);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+        ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
-            
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
      );
 }
