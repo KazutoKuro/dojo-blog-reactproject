524f71f91034c3cc42782b6ccd6cd5ee03bc365a
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-14 21:19:42 +0630

    Reusing Components

--- src/Home.js
+++ src/Home.js
@@ -93,6 +93,29 @@
 
 //vid-11 - Props
 
+// import { useState } from 'react';
+// import BlogList from './BlogList';
+
+// const Home = () => {
+//     const [blogs, setBlogs] = useState([
+//         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+//         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+//         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+//         ]);
+
+//     return ( 
+//         <div className="home">
+//             <BlogList blogs={blogs} title="All Blogs!" /> 
+//         </div>
+//      );
+// }
+ 
+// export default Home;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//vid-12 - Reusing Components
+
 import { useState } from 'react';
 import BlogList from './BlogList';
 
@@ -106,6 +129,7 @@ const Home = () => {
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" /> 
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's blogs" /> 
         </div>
      );
 }
