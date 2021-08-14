aac63a7501bad74e1c804fb56b69b2a9ef69478b
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-14 20:29:38 +0630

    Props

--- src/Home.js
+++ src/Home.js
@@ -66,7 +66,35 @@
 
 //vid-10 - Outputting Lists
 
+// import { useState } from 'react';
+
+// const Home = () => {
+//     const [blogs, setBlogs] = useState([
+//         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+//         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+//         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+//         ]);
+
+//     return ( 
+//         <div className="home">
+//             {blogs.map((blog) => (
+//                 <div className="blog-preview" key={blog.id}>
+//                     <h2>{ blog.title }</h2>
+//                     <p>Written by { blog.author }</p>
+//                 </div>
+//             ))}
+//         </div>
+//      );
+// }
+ 
+// export default Home;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//vid-11 - Props
+
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -77,12 +105,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs!" /> 
         </div>
      );
 }
