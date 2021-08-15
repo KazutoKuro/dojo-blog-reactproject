591d6f0f1b702592961f4bba357cfb6170d68a07
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-15 22:40:00 +0630

    Making a Custom Hook

--- src/Home.js
+++ src/Home.js
@@ -306,34 +306,56 @@
 
 //vid-19 - Handling Fetch Errors
 
+// import { useState, useEffect } from 'react';
+// import BlogList from './BlogList';
+
+// const Home = () => {
+//     const [blogs, setBlogs] = useState(null);
+//     const [isPending, setIsPending] = useState(true);
+//     const [error, setError] = useState(null);
+
+//     useEffect(() => {
+//         setTimeout(() => {
+//             fetch('http://localhost:8000/blogs')
+//             .then(res => {
+//                 if(!res.ok){
+//                     throw Error('Could not fetch the data for that resource');
+//                 }
+//                 return res.json();
+//             })
+//             .then(data => {
+//                 setBlogs(data);
+//                 setIsPending(false);
+//                 setError(null);
+//             })
+//             .catch((err) => {
+//                 setIsPending(false);
+//                 setError(err.message);
+//             })
+//         }, 1000);
+//     }, []); 
+
+//     return ( 
+//         <div className="home">
+//             { error && <div> { error } </div> }
+//             { isPending && <div>Loading....</div> }
+//             { blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
+//         </div>
+//      );
+// }
+ 
+// export default Home;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//vid-20 - Making a Custom Hook
+
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if(!res.ok){
-                    throw Error('Could not fetch the data for that resource');
-                }
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch((err) => {
-                setIsPending(false);
-                setError(err.message);
-            })
-        }, 1000);
-    }, []); 
+    const{ data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
     return ( 
         <div className="home">
