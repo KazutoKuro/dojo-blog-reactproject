b900b0f6cd896cab7039962391046c0de6572cd3
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-15 22:15:49 +0630

    Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -241,29 +241,63 @@
 
 //vid-17 - Fetching data with useEffect
 
+// import { useState, useEffect } from 'react';
+// import BlogList from './BlogList';
+
+// const Home = () => {
+//     const [blogs, setBlogs] = useState(null);
+
+//     useEffect(() => {
+//         fetch('http://localhost:8000/blogs')
+//             .then(res => {
+//                 return res.json();
+//             })
+//             .then(data => {
+//                 setBlogs(data);
+//             })
+//     }, []); 
+
+//     return ( 
+//         <div className="home">
+//             { blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
+//         </div>
+//      );
+// }
+ 
+// export default Home;
+
+// Note: $ npx json-server --watch data/db.json --port 8000
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//vid-18 - Conditional Loading Message
+
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
-            })
+                setIsPending(false);
+            });
+        }, 1000);
     }, []); 
 
     return ( 
         <div className="home">
+            { isPending && <div>Loading....</div> }
             { blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
         </div>
      );
 }
  
 export default Home;
-
-// Note: $ npx json-server --watch data/db.json --port 8000
