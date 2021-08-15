802952326d6e9b9df5db6a79ca41e5f3f8791e6c
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-15 22:10:39 +0630

    Fetching data with useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -23,7 +23,30 @@
 
 //Vid-13 - Function as Props
 
-const BlogList = ({blogs, title, handleDelete}) => {
+// const BlogList = ({blogs, title, handleDelete}) => {
+
+    
+//     return ( 
+//         <div className="blog-list">
+//             <h2> { title } </h2>
+//             {blogs.map((blog) => (
+//                 <div className="blog-preview" key={blog.id}>
+//                     <h2>{ blog.title }</h2>
+//                     <p>Written by { blog.author }</p>
+//                     <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+//                 </div>
+//             ))}
+//         </div>
+//      );
+// }
+ 
+// export default BlogList;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//Vid-17 - Fetching data with useEffect
+
+const BlogList = ({blogs, title }) => {
 
     
     return ( 
@@ -33,7 +56,6 @@ const BlogList = ({blogs, title, handleDelete}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -239,35 +239,27 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-//vid-16 - Using Json Server
+//vid-17 - Fetching data with useEffect
 
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-        ]);
-
-    const [name, setName] = useState('mario');
-        
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id );
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('use effect ran');
-        console.log(name);
-    }, [name]); 
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
+    }, []); 
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/> 
-            <button onClick={() => setName('luigi')} >change name</button>
-            <p>{ name }</p>
+            { blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
         </div>
      );
 }
