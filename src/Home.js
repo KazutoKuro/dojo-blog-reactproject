c009a73695555bdda8b9248c87b682979d3b799c
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-15 22:25:38 +0630

    Handling Fetch Errors

--- src/Home.js
+++ src/Home.js
@@ -272,28 +272,72 @@
 
 //vid-18 - Conditional Loading Message
 
+// import { useState, useEffect } from 'react';
+// import BlogList from './BlogList';
+
+// const Home = () => {
+//     const [blogs, setBlogs] = useState(null);
+//     const [isPending, setIsPending] = useState(true);
+
+//     useEffect(() => {
+//         setTimeout(() => {
+//             fetch('http://localhost:8000/blogs')
+//             .then(res => {
+//                 return res.json();
+//             })
+//             .then(data => {
+//                 setBlogs(data);
+//                 setIsPending(false);
+//             });
+//         }, 1000);
+//     }, []); 
+
+//     return ( 
+//         <div className="home">
+//             { isPending && <div>Loading....</div> }
+//             { blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
+//         </div>
+//      );
+// }
+ 
+// export default Home;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//vid-19 - Handling Fetch Errors
+
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok){
+                    throw Error('Could not fetch the data for that resource');
+                }
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
                 setIsPending(false);
-            });
+                setError(null);
+            })
+            .catch((err) => {
+                setIsPending(false);
+                setError(err.message);
+            })
         }, 1000);
     }, []); 
 
     return ( 
         <div className="home">
+            { error && <div> { error } </div> }
             { isPending && <div>Loading....</div> }
             { blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
         </div>
