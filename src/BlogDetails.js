0e2f85cbf49388e38e95ad74f149b0375b070358
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-16 22:44:01 +0630

    Reusing custom Hooks

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,41 @@
 // vid-25 - Route Parameters
 
+// import { useParams } from "react-router-dom";
+
+// const BlogDetails = () => {
+//     const { id } = useParams(); // allow us to grab parameters or route parameters
+
+//     return ( 
+//         <div className="blog-details">
+//             <h2>Blog details - { id }</h2>
+//         </div>
+//      );
+// }
+ 
+// export default BlogDetails;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// vid-26 - Reusing Custom Hooks
+
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams(); // allow us to grab parameters or route parameters
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
 
     return ( 
         <div className="blog-details">
-            <h2>Blog details - { id }</h2>
+            { isPending && <div> Loading... </div>}
+            { error && <div>{ error }</div>}
+            { blog && (
+                <article>
+                    <h2>{ blog.title} </h2>
+                    <p>Written by { blog.author }</p>
+                    <div>{ blog.body}</div>
+                </article>
+            )}
         </div>
      );
 }
