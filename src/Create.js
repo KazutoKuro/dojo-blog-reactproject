19ad9d7c3db8c100c9e451b4e3a92522c8a0e1b3
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-16 23:13:48 +0630

    Submit Events

--- src/Create.js
+++ src/Create.js
@@ -13,6 +13,54 @@
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 // Vid-27 - Controlled Inputs(forms)
+
+// import { useState } from "react";
+
+// const Create = () => {
+//     const [title, setTitle] = useState('');
+//     const [body, setBody] = useState('');
+//     const [author, setAuthor] = useState('mario');
+
+//     return (
+//         <div className="create">
+//             <h2>Add a New Blog</h2>
+//             <form>
+//                 <label>Blog title: </label>
+//                 <input 
+//                     type="text"
+//                     required
+//                     value={title}
+//                     onChange={(e) => setTitle(e.target.value)}
+//                 />
+//                 <label>Blog body: </label>
+//                 <textarea 
+//                     required
+//                     value={body}
+//                     onChange={(e) => setBody(e.target.value)}
+//                 />
+//                 <label>Blog author:</label>
+//                 <select
+//                     value={author}
+//                     onChange={(e) => setAuthor(e.target.value)}
+//                 >
+//                     <option value="mario">mario</option>
+//                     <option value="yoshi">yoshi</option>
+//                 </select>
+//                 <button>Add Blog</button>
+//                 <p>{ title }</p>
+//                 <p>{ body }</p>
+//                 <p>{ author }</p> 
+//             </form>
+//         </div>
+//     );
+// }
+ 
+// export default Create;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Vid-28 - Submit Events
+
 import { useState } from "react";
 
 const Create = () => {
@@ -20,10 +68,16 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+        console.log(blog);
+    }
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title: </label>
                 <input 
                     type="text"
