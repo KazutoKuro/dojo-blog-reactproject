d46ecd2db97b1487d11be0930a9367ac332ff6c1
KauztoKuro <ikkikurogane70@gmail.com>
2021-08-16 20:05:36 +0630

    Router Links

--- src/App.js
+++ src/App.js
@@ -107,6 +107,38 @@
 
 // Vid-22 - Exact Match Routes
 
+// import Navbar from './Navbar';
+// import Home from './Home';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Create from './Create';
+
+// function App() {
+
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Navbar />
+//         <div className="content">
+//           <Switch>
+//             <Route exact path="/"> 
+//               <Home />
+//             </Route>
+//             <Route path="/create"> 
+//               <Create />
+//             </Route>
+//           </Switch>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Vid-23 - Router Links
+
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
--- src/Navbar.js
+++ src/Navbar.js
@@ -44,13 +44,33 @@
 
 //vid-7 - click Events
 
+// const Navbar = () => {
+//     return ( 
+//         <nav className="navbar">
+//             <h1>The Dojo Blog</h1>
+//             <div className="links">
+//                 <a href="/">Home</a>
+//                 <a href="/create">New Blog</a>     
+//             </div>
+//         </nav>
+//      );
+// }
+ 
+// export default Navbar ;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//vid-23 - Router Links
+
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>     
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>     
             </div>
         </nav>
      );
